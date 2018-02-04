e297ad9c7eae20961022143d7e346e5b1119c13f
ckdoan <ckdoan@ucalgary.ca>
2018-02-03 18:33:41 -0700

    added frequency function

--- Assign2/code.js
+++ Assign2/code.js
@@ -101,21 +101,10 @@ function getStats(txt) {
       return listofwords;
     }
 
-    //sorts list alphabettically
-    function sortlist(arr){
-
-    }
-
-    function doesnotcontain(word, arr) {
-      for (i = 0; i < arr.length; i++) {
-          if (arr[i] === word) {
-              return true;
-          }
-      }
-      return false;
-    }
 
     //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-an-array-remove-duplicates
+
+    //ASK TA
     function onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
     }
@@ -123,46 +112,124 @@ function getStats(txt) {
 
 
     function longestWords_func(txt){
-
-      let wordlist = [];
-
       let wordarray = txt.replace(/\W+/g, " ").trim().toLowerCase().split(" ");
-
-
       let uniquearray = wordarray.filter(onlyUnique);
-
       let sorted = uniquearray.sort(function(a, b){
                return b.length - a.length;
       });
 
-      print("sorted" + sorted);
       let topten = [];
       let temparr = [];
-      for (i = 0; i<sorted.length-1 ;i++ ){
-        if(sorted[i].length === sorted[i+1].length){
+//      print ("sorted " + sorted  );
+      for (i = 0; i<sorted.length  ;i++ ){
+//        print ("sorted[i]" + sorted[i]);
+        if(i ===sorted.length-1 ){
+          topten.push(sorted[i])
+        }
+        else if(sorted[i].length === sorted[i+1].length){
           temparr.push(sorted[i])
+//          print("in here");
         }
         //ie. only 1 element with that length
-        else if(sorted[i].length !== sorted[i+1].length){
+        else if(sorted[i].length !== sorted[i+1].length ){
+    //      print("in here2 2");
           temparr.push(sorted[i]);
           let temp2 = temparr.sort();
-          Array.prototype.push.apply(topten, temp2);
+      //    Array.prototype.push.apply(topten, temp2); // ask TA
+          topten = topten.concat(temp2);
 //          topten.push(temp2);
           temparr = [];
           temp2 = [];
         }
-
       }
-
+  //    print("loop end");
       let topten2 = [];
-      for ( i = 0 ; i<10; i++){
+      for ( i = 0 ; i<topten.length; i++){
         topten2.push(topten[i]);
+      //  print("heaf " + topten[i])
+        if(i===9){
+          break;
+        }
       }
       return topten2;
 
     }
 
     function mostFrequentWords_func(txt){
+      let wordarray = txt.replace(/\W+/g, " ").trim().toLowerCase().split(" ");
+      let wordcounts = {};
+      for(i = 0; i < wordarray.length; i++){
+          if (  wordcounts[wordarray[i]] ===undefined){
+            wordcounts[wordarray[i]] = 1;
+          }
+          else{
+            wordcounts[wordarray[i]] = wordcounts[wordarray[i]] + 1;
+          }
+      }
+      /* https://stackoverflow.com/questions/25500316/sort-a-dictionary-by-value-in-javascript*/
+      var items = Object.keys(wordcounts).map(function(key) {
+          return [key, wordcounts[key]];
+      });
+
+      // Sort the array based on the second element
+      items.sort(function(first, second) {
+          return second[1] - first[1];
+      });
+
+      print('items', items);
+      let count = 0 ;
+      let newarr = [];
+      let final = [];
+      let freqcount = 0 ;
+      let index = 0 ;
+      print('done');
+      for (i = 0; i<items.length; i++ ) {
+        if (count ===0 ){
+          freqcount = items[i][1];
+  //        print('here' , freqcount);
+
+          newarr.push(items[i][0] + "(" + freqcount + ")");
+        }
+        else if (count !==0){
+  //        print('in here');
+          if(items[i][1] === freqcount){
+            newarr.push(items[i][0]+ "(" + freqcount + ")");
+  //          print('same');
+          }
+          else if (items[i][1] !== freqcount){
+      //      print('doff');
+            newarr.sort();
+        /*    if(index === 0){
+              final.push(newarr);
+              index++;
+            }
+            if(index !== 0){
+
+
+            }*/
+
+            final = final.concat(newarr);
+      //      print('newarr ', newarr, 'final'+ final);
+            freqcount= items[i][1]
+            newarr = [];
+            newarr.push(items[i][0] + "(" + freqcount + ")");
+          }
+        }
+        count++;
+        print('out');
+      }
+      print('fina', final);
+
+      let topten = [];
+      for ( i = 0 ; i<final.length; i++){
+        topten.push(final[i]);
+      //  print("heaf " + topten[i])
+        if(i===9){
+          break;
+        }
+      }
+      return topten;
+
 
     }
 
@@ -175,6 +242,6 @@ function getStats(txt) {
         averageWordLength: averageWordLength_func(txt),
         palindromes: palindromes_func(txt),
         longestWords: longestWords_func(txt),
-        mostFrequentWords: "asd"
+        mostFrequentWords: mostFrequentWords_func(txt)
     };
 }
